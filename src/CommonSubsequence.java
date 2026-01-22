822c02f52dae15fcf512c928637dc9d913753a4f
xavierroelandts <xkroelandts@mail.com>
2026-01-22 14:10:08 -0600

    Added the beginning of the code for thr CommonSubstring

--- src/CommonSubsequence.java
+++ src/CommonSubsequence.java
@@ -17,25 +17,25 @@ public class CommonSubsequence {
         int m = A.length;
         int n = B.length;
 
-        int [][] x = new int[m +1][n +1];
+        int [][] dp = new int[m +1][n +1];
 
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
 
                 if (A[i - 1] == B[j - 1]) {
-                    x[i][j] = x[i - 1][j - 1] + 1;
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
                 } else {
-                    if (x[i - 1][j] > x[i][j - 1]) {
-                        x[i][j] = x[i - 1][j];
+                    if (dp[i - 1][j] > dp[i][j - 1]) {
+                        dp[i][j] = dp[i - 1][j];
                     } else {
-                        x[i][j] = x[i][j - 1];
+                        dp[i][j] = dp[i][j - 1];
                     }
                 }
 
             }
         }
 
-        return x[m][n];
+        return dp[m][n];
     }
 
 }
--- src/CommonSubstring.java
+++ src/CommonSubstring.java
@@ -1,2 +1,26 @@
 public class CommonSubstring {
+
+    public static void main(String[] args) {
+        String s1 = "spy family";
+        String s2 = "jujutzu";
+
+    }
+
+    public static String longestCommonSubstring(String text1, String text2) {
+        int m = text1.length();
+        int n = text2.length();
+        int maxLength = 0;
+        int endIndex = 0;
+
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 1; i <= m; i++){
+            for (int j = 1; j <= n; j++){
+
+            }
+        }
+
+        return;
+    }
+
 }
