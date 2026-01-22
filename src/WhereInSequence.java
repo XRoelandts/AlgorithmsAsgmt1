9c0ee717caca0bff7112f764afafb33b19e5dfb7
xavierroelandts <xkroelandts@mail.com>
2026-01-22 14:36:45 -0600

    Added the main methods and body of them to the WhereInSequence

--- src/WhereInSequence.java
+++ src/WhereInSequence.java
@@ -1,2 +1,24 @@
 public class WhereInSequence {
+
+    public static void main(String[] args){
+        long target1 = 8;
+
+        long target2 = 1763;
+    }
+
+    public static int findPositionInNotFibonacci(long target) {
+        long prev2 = 0;
+        long prev1 = 2;
+        int position = 1;
+
+        if (target <= 0){
+            return 0;
+        } else if (target < 2){
+            return 0;
+        }else if (target == 2){
+            return 1;
+        }
+    }
+
+
 }
